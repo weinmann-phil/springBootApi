af1fd3ac081bc3735c04144a108147cf1b7a1b2c
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-02-09 17:33:16 +0100

    chore(*): add Rest options

--- src/main/java/com/example/springBootApi/SpringBootApiApplication.java
+++ src/main/java/com/example/springBootApi/SpringBootApiApplication.java
@@ -2,12 +2,20 @@ package com.example.springBootApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApiApplication.class, args);
 	}
+
+  @GetMapping
+  public List<String> hello() {
+    return List.of("Hello", "World!");
+  }
 
 }
