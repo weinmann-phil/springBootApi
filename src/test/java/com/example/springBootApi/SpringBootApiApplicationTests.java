e90f196abec1310d79ac331d846077e0a90cde93
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-02-09 14:46:37 +0100

    Init commit

---
+++ src/test/java/com/example/springBootApi/SpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
